c484cf4e2a8f9e7b2f29b5495674fd7183db3451
mrverdant13 <karlo.verde.1996@gmail.com>
2020-12-29 03:28:32 -0500

    Change base colors

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col h-screen">
+  <div class="flex flex-col h-screen bg-gray-800 text-white">
     <AppHeader />
-    <div class="flex-grow">
+    <div class="flex-grow bg-gray-900 text-white">
       <router-view />
     </div>
   </div>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav id="app-header" class="flex p-2 w-full bg-black">
+  <nav id="app-header" class="flex p-2 w-full">
     <router-link
       v-for="route in $router.getRoutes()"
       :key="route.name"
       :to="route.path"
-      class="px-2 py-1 text-white"
+      class="px-2 py-1"
       >{{ route.name }}</router-link
     >
   </nav>
