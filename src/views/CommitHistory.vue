ca2c47ad96dafd2ac8f1bad0d968c602149a4fee
mrverdant13 <karlo.verde.1996@gmail.com>
2020-12-30 18:33:06 -0500

    Use repo branches for dropdown items
    Using Github API v3 version.
    
    Ref: https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#list-branches
    
    Github repo owner and repo name provided by .env.local file.

--- src/views/CommitHistory.vue
+++ src/views/CommitHistory.vue
@@ -1,13 +1,55 @@
 <template>
-  <div id="commit-history" class="flex h-full w-full">
-    <div class="m-auto">
-      <h1>Commit History</h1>
+  <div id="commit-history" class="flex h-full w-full p-6">
+    <div class="w-36">
+      <Dropdown
+        @item-selected="onBranchSelected"
+        hint="branch"
+        :items="branchesData"
+        :itemLabelBuilder="(item) => item.name"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import { onMounted, ref } from "vue";
+
+import axios from "axios";
+
+import Dropdown from "../components/Dropdown";
+
 export default {
   name: "CommitHistory",
+
+  components: { Dropdown },
+
+  setup() {
+    // Data
+    const branchesData = ref([]);
+
+    // Methods
+    function onBranchSelected(branchData) {
+      console.log(branchData);
+    }
+    async function updateBranches() {
+      const { data } = await axios.get(
+        `https://api.github.com/repos/${process.env.VUE_APP_REPO_OWNER}/${process.env.VUE_APP_REPO_NAME}/branches`
+      );
+      branchesData.value = data;
+    }
+
+    // Lifecycle Hooks
+    onMounted(() => {
+      updateBranches();
+    });
+
+    return {
+      // Data
+      branchesData,
+      // Methods
+      onBranchSelected,
+      updateBranches,
+    };
+  },
 };
 </script>
