6fb327c1560592b472f7b66f410224fa0687fa64
mrverdant13 <karlo.verde.1996@gmail.com>
2020-12-31 02:40:03 -0500

    Create commits list component
    Using CommitTile for each commit

---
+++ src/components/CommitsList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-gray-800 border border-gray-600 rounded">
+    <div
+      v-for="commitData in commitsData"
+      :key="commitData"
+      class="border-b border-gray-600 px-4 py-2 last:border-0"
+    >
+      <CommitTile :commitData="commitData" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CommitTile from "../components/CommitTile";
+
+export default {
+  name: "CommitsList",
+
+  components: {
+    CommitTile,
+  },
+
+  props: {
+    commitsData: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
