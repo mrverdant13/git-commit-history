91f8fc97ed12779f618d0706d60cbfc6f5579187
mrverdant13 <karlo.verde.1996@gmail.com>
2020-12-31 04:38:08 -0500

    Copy commit sha on click

--- src/components/CommitTile.vue
+++ src/components/CommitTile.vue
@@ -1,36 +1,49 @@
 <template>
-  <div class="flex flex-col">
-    <div class="flex flex-wrap">
-      <p class="font-semibold text-gray-200 leading-tight mb-1 text-sm">
-        <span>{{ commitTitle }}&nbsp;&nbsp;</span>
-        <button
-          v-if="commitDescription !== null"
-          @click="commitDescriptionIsOpen ^= true"
-          type="button"
-          class="inline-flex justify-center items-center bg-gray-700 hover:bg-gray-600 focus:outline-none px-1 rounded-sm"
-        >
-          <div class="h-2"></div>
-          <p class="relative -inset-y-2 h-2.5">...</p>
-        </button>
+  <div class="flex flex-row place-items-start">
+    <div class="flex flex-col flex-grow">
+      <div class="flex flex-wrap">
+        <p class="font-semibold text-gray-200 leading-tight mb-1 text-sm">
+          <span>{{ commitTitle }}&nbsp;&nbsp;</span>
+          <button
+            v-if="commitDescription !== null"
+            @click="commitDescriptionIsOpen ^= true"
+            type="button"
+            class="inline-flex justify-center items-center bg-gray-700 hover:bg-gray-600 focus:outline-none px-1 rounded-sm"
+          >
+            <div class="h-2"></div>
+            <p class="relative -inset-y-2 h-2.5">...</p>
+          </button>
+        </p>
+      </div>
+      <p
+        v-if="commitDescriptionIsOpen"
+        class="text-xs font-mono mb-2 text-gray-400 break-all whitespace-pre-wrap sm:break-normal"
+      >
+        {{ commitDescription }}
       </p>
-    </div>
-    <p
-      v-if="commitDescriptionIsOpen"
-      class="text-xs font-mono mb-2 text-gray-400 break-all whitespace-pre-wrap sm:break-normal"
-    >
-      {{ commitDescription }}
-    </p>
 
-    <div class="flex">
-      <img :src="committerAvatarUrl" class="h-5 rounded-full w-5" />
-      <div class="text-gray-400 text-xs mx-2 flex flex-wrap">
-        <div class="font-semibold text-gray-200">{{ committerName }}&nbsp;</div>
-        <div>committed&nbsp;</div>
-        <div :title="commitDateTime.toString()">
-          {{ commitDateDiffMessage }}
+      <div class="flex">
+        <img :src="committerAvatarUrl" class="h-5 rounded-full w-5" />
+        <div class="text-gray-400 text-xs mx-2 flex flex-wrap">
+          <div class="font-semibold text-gray-200">
+            {{ committerName }}&nbsp;
+          </div>
+          <div>committed&nbsp;</div>
+          <div :title="commitDateTime.toString()">
+            {{ commitDateDiffMessage }}
+          </div>
         </div>
       </div>
     </div>
+    <button
+      :title="`Copy ${commitSha} sha`"
+      @click="copyShaToClipboard"
+      class="bg-gray-900 hover:bg-gray-700 invisible w-0 sm:w-auto sm:visible rounded-lg border border-gray-600 p-2 focus:outline-none"
+    >
+      <p class="text-xs text-blue-400">
+        {{ commitSha.substring(0, 6) }}
+      </p>
+    </button>
   </div>
 </template>
 
@@ -120,6 +133,21 @@ export default {
         }
       },
     });
+    const commitSha = computed({
+      get: () => {
+        return props.commitData.sha;
+      },
+    });
+
+    // Methods
+    function copyShaToClipboard() {
+      var dummy = document.createElement("textarea");
+      document.body.appendChild(dummy);
+      dummy.value = commitSha.value;
+      dummy.select();
+      document.execCommand("copy");
+      document.body.removeChild(dummy);
+    }
 
     return {
       // Data
@@ -131,6 +159,9 @@ export default {
       committerName,
       commitDateTime,
       commitDateDiffMessage,
+      commitSha,
+      // Methods
+      copyShaToClipboard,
     };
   },
 };
